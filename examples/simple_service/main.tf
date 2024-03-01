a9c1cb3114646150ec5edb37907992de8e128811
keimille <keith@mediavine.com>
2024-03-01 09:05:09 -0500

    Adds options to enable and disable headers and request settings with default values (can be overwritten).

--- examples/simple_service/main.tf
+++ examples/simple_service/main.tf
@@ -1,14 +1,16 @@
 module "fastly_demo" {
-  source                = "../.."
-  fastly_api_key        = "yourkeyhere" #set as envar and do not hardcode
-  fastly_force_destroy  = true
-  fastly_service_name   = "example-service"
-  fastly_domain_name    = "www.example.com"
-  fastly_domain_comment = "This is an example domain"
+  source                         = "../.."
+  fastly_api_key                 = "yourkeyhere"
+  fastly_force_destroy           = true
+  fastly_service_name            = "example-service"
+  fastly_domain_name             = "www.example.com"
+  fastly_domain_comment          = "This is an example domain"
+  fastly_enable_header           = true
+  fastly_enable_request_settings = true
   fastly_backends = [
     {
       name                  = "example-backend"
-      address               = "example.com"
+      address               = "fides.mediavine.com"
       port                  = 80
       auto_loadbalance      = false
       between_bytes_timeout = 10000
--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
+locals {
+  default_header = {
+    name          = var.fastly_header_name
+    action        = var.fastly_header_action
+    destination   = var.fastly_header_destination
+    ignore_if_set = var.fastly_header_ignore_if_set
+    priority      = var.fastly_header_priority
+    source        = var.fastly_header_source
+    type          = var.fastly_header_type
+  }
+}
+
 resource "fastly_service_vcl" "this" {
   name = var.fastly_service_name
 
-  activate      = var.fastly_activate
-  force_destroy = var.fastly_force_destroy
+  activate       = var.fastly_activate
+  force_destroy  = var.fastly_force_destroy
+  stale_if_error = var.fastly_stale_if_error
 
   domain {
     name    = var.fastly_domain_name
@@ -25,6 +38,32 @@ resource "fastly_service_vcl" "this" {
       use_ssl               = backend.value.use_ssl
       weight                = backend.value.weight
     }
+  }
 
+  dynamic "header" {
+    for_each = var.fastly_enable_header ? (length(var.headers) > 0 ? var.headers : [local.default_header]) : []
+
+    content {
+      name          = header.value.name
+      action        = header.value.action
+      destination   = header.value.destination
+      ignore_if_set = header.value.ignore_if_set
+      priority      = header.value.priority
+      source        = header.value.source
+      type          = header.value.type
+    }
+  }
+
+  dynamic "request_setting" {
+    for_each = var.fastly_enable_request_settings ? [1] : []
+
+    content {
+      name             = var.fastly_request_setting_name
+      bypass_busy_wait = var.fastly_request_setting_bypass_busy_wait
+      force_miss       = var.fastly_request_setting_force_miss
+      force_ssl        = var.fastly_request_setting_force_ssl
+      max_stale_age    = var.fastly_request_setting_max_stale_age
+      timer_support    = var.fastly_request_setting_timer_support
+    }
   }
 }
